93c87c10897ef45a4b83cd4426ce1dad5867bea2
Xuzi <2064399224@qq.com>
2024-09-28 17:16:00 +0800

    Update main.py

--- 3122004797/PairingProject/main.py
+++ 3122004797/PairingProject/main.py
@@ -2,6 +2,7 @@ import argparse
 import Evaluate
 import Generate
 import cProfile
+import sys
 import pstats
 import io
 import matplotlib.pyplot as plt
@@ -24,11 +25,20 @@ def main():
     pr.enable()  # 开始性能分析
 
     if args.n is not None and args.r is not None:
+        # 检查参数是否大于0
+        if args.n <= 0:
+            print("错误：参数 n 必须大于 0。")
+        if args.r < 0:
+            print("错误：参数 r 必须大于或等于 0。")
+            sys.exit(1)
+
+
         # 生成题目
         exercises = Generate.generate_exercises(args.n, args.r)
         Generate.write_to_file("Exercises.txt", [f"{q[1]}" for q in exercises])
         Generate.write_to_file("Answers.txt", [q[0] for q in exercises])
         print(f"已生成 {args.n} 道题目，保存在 Exercises.txt 和 Answers.txt 中。")
+
     elif args.e is not None and args.a is not None:
         # 评分
         correct, wrong = Evaluate.grade_exercises(args.e, args.a)
@@ -43,12 +53,14 @@ def main():
             file.write(f"Wrong: {wrong_count} ({', '.join(map(str, wrong))})\n")
 
         print(f"评分结果已保存到 Grade.txt 中。")
+
     else:
         print("请提供有效的参数。")
 
     pr.disable()  # 停止性能分析
     # 将性能分析结果保存到二进制文件
     pr.dump_stats("profile_data.prof")  # 保存为 .prof 文件
+
 
 if __name__ == "__main__":
     main()
